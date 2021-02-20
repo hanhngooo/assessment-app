dfe90d219d78aa2dbc7eccad147b709f22b03a50
hanhngooo <hanhngo1795@gmail.com>
2021-02-20 19:11:02 +0100

    implement hooks from downshigt for search-multiselect bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddText from "./components/AddText";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Question 1: Add Text</h3>
+      <AddText />
     </div>
   );
 }
---
+++ src/components/AddText.js
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import { useCombobox, useMultipleSelection } from "downshift";
+
+export default function AddText() {
+  const items = [
+    "Close Account",
+    "Open Account",
+    "Password",
+    "Shut Account",
+    "Sue",
+    "Terminate",
+    "Update Account",
+  ];
+  const [inputValue, setInputValue] = useState("");
+  const {
+    getDropdownProps,
+    addSelectedItem,
+    removeSelectedItem,
+    selectedItems,
+  } = useMultipleSelection();
+  const getFilteredItems = () =>
+    items.filter((item) =>
+      item.toLowerCase().includes(inputValue.toLowerCase())
+    );
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    inputValue,
+    defaultHighlightedIndex: 0, // after selection, highlight the first item.
+    selectedItem: null,
+    items: getFilteredItems(),
+    stateReducer: (state, actionAndChanges) => {
+      const { changes, type } = actionAndChanges;
+      switch (type) {
+        case useCombobox.stateChangeTypes.InputKeyDownEnter:
+        case useCombobox.stateChangeTypes.ItemClick:
+          return {
+            ...changes,
+            isOpen: true, // keep the menu open after selection.
+          };
+      }
+      return changes;
+    },
+    onStateChange: ({ inputValue, type, selectedItem }) => {
+      switch (type) {
+        case useCombobox.stateChangeTypes.InputChange:
+          setInputValue(inputValue);
+          break;
+        case useCombobox.stateChangeTypes.InputKeyDownEnter:
+        case useCombobox.stateChangeTypes.ItemClick:
+        case useCombobox.stateChangeTypes.InputBlur:
+          if (!selectedItems.includes(selectedItem)) {
+            addSelectedItem(selectedItem);
+          } else {
+            removeSelectedItem(selectedItem);
+          }
+          break;
+        default:
+          break;
+      }
+    },
+  });
+  return (
+    <div>
+      <p>input: {inputValue}</p>
+      <p>selected: {selectedItems}</p>
+
+      <div {...getComboboxProps()}>
+        <input
+          {...getInputProps(getDropdownProps(), {
+            onChange: (e) => setInputValue(e.target.value),
+          })}
+          placeholder="Add Text Pattern..."
+        />
+        <button
+          type="button"
+          {...getToggleButtonProps()}
+          aria-label="toggle menu"
+        >
+          &#8595;
+        </button>
+        <button>Insert</button>
+      </div>
+      <ul {...getMenuProps()}>
+        {isOpen &&
+          getFilteredItems(items).map((item, index) => (
+            <li
+              style={
+                highlightedIndex === index ? { backgroundColor: "#bde4ff" } : {}
+              }
+              key={`${item}${index}`}
+              {...getItemProps({
+                item,
+                index,
+              })}
+            >
+              <input
+                type="checkbox"
+                checked={selectedItems.includes(item)}
+                value={item}
+                onChange={() => null}
+              />
+              <span />
+              {item}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
